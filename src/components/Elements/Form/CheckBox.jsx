448e4854bb73f6291274e082174a044f46ca7c0e
Dipankar Halder <dipankar.halder@borngroup.com>
2021-03-29 14:51:29 +0530

    not completed checkbox for now

--- src/components/Elements/Form/CheckBox.jsx
+++ src/components/Elements/Form/CheckBox.jsx
@@ -1,7 +1,49 @@
-export default function CheckBox() {
+import PropTypes from 'prop-types';
+
+export default function CheckBox({
+  divClass,
+  checkClass,
+  selected,
+  label,
+  onChange
+}) {
+
+  const onChangeHandle = (event) => {
+    const { selected } = event.target;
+    onChange(selected);
+  }
+
   return (
-    <div>
-      
+    <div className={divClass}>
+      <label className={checkClass}>
+        <input 
+          type="checkbox"
+          value={selected}
+          defaultChecked={selected}
+          onChange={onChangeHandle}
+        />
+        <span>{label}</span>
+      </label>
     </div>
   )
+}
+
+/***********************************
+  @usage: Checking Prop Type,
+  @packages: prop-types
+***********************************/
+CheckBox.propTypes = {
+  divClass: PropTypes.string,
+  checkClass: PropTypes.string,
+  selected: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+/*************************************
+  @usage: added initial props value
+*************************************/
+CheckBox.defaultProps = {
+  divClass: 'app-input-field-check',
+  dropClass: 'app-checkbox'
 }
